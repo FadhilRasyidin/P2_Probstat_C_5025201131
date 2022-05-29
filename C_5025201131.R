6bd226a826963a014af8bf988d5e8d03ca1995f6
Mohammad Fadhil Rasyidin Parinduri <dhyni1022@gmail.com>
2022-05-29 23:51:43 +0700

    added numero tres cuatro cinco

--- C_5025201131.R
+++ C_5025201131.R
@@ -65,3 +65,86 @@ qt(p = 0.05, df = 2, lower.tail = FALSE)
 #lower.tail = FALSE karena kalau TRUE minus
 
 #e & f ada di readme
+
+############ Nomor 4
+#a
+
+myFile  <- read.table(url("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt"))
+dim(myFile)
+head(myFile)
+attach(myFile)
+
+myFile$V1 <- as.factor(myFile$V1)
+myFile$V1 = factor(myFile$V1,labels = c("Kucing Oren","Kucing Hitam","Kucing Putih","Kucing Oren"))
+
+class(myFile$V1)
+
+group1 <- subset(myFile, V1=="Kucing Oren")
+group2 <- subset(myFile, V1=="Kucing Hitam")
+group3 <- subset(myFile, V1=="Kucing Putih")
+
+#b
+bartlett.test(Length~V1, data=dataoneway)
+
+#c
+qqnorm(group1$Length)
+qqline(group1$Length)
+
+#d di README
+
+#e
+model1 <- lm(Length~Group, data=myFile)
+
+anova(model1)
+
+TukeyHSD(aov(model1))
+
+#f
+library(ggplot2)
+ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + 
+  scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
+
+
+############ Nomor 5
+#a
+install.packages("multcompView")
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+#b
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+#c
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+#d
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+#e
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+cld <- as.data.frame.list(tukey.cld$`Glass:Temp_Factor`)
+data_summary$Tukey <- cld$Letters
+print(data_summary)
+
+write.csv("GTL_summary.csv")
